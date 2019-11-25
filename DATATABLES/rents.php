01d0a1cb62a44c2ca7d4e73bcd16e2ff1ad071c1
josep <josep.oliverr@gmail.com>
2019-11-25 09:26:34 +0100

    DataTables

--- DATATABLES/rents.php
+++ DATATABLES/rents.php
@@ -1,7 +1,7 @@
 <?php
 header("Content-Type: application/json; charset=UTF-8");
 $conn = new mysqli("localhost", "josep", "123", "Cronose");
-$stmt = $conn->prepare("select id,user.username as name,email,offer.Title as job,Valoration_Average as val1, Personal_Valoration as val2, offer.Description as description, Coin_Price as price from user,offer where user.id = offer.User_Id");
+$stmt = $conn->prepare("select id,username as name,email,offer.Title as job,Valoration_Average as val1, Personal_Valoration as val2, offer.Description as description, Coin_Price as price from User,offer where User.id = offer.User_Id");
 $stmt->execute();
 $result = $stmt->get_result();
 $outp = $result->fetch_all(MYSQLI_ASSOC);
